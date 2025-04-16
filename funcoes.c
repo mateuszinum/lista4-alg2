77851b4073d9bcbc5765607a951ac401b515becd
Mateus A. Muniz S. <mateuszinum@gmail.com>
2025-04-15 21:12:35 -0400

    Add files via upload

--- funcoes.c
+++ funcoes.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 struct TNo {
     int info;         
     struct TNo *prox;     
 };
+
+struct Tno {
+	int info;
+	struct Tno *esq;
+	struct Tno *dir;
+};
+
 
 void insere_ordenado(struct TNo **l, int x) {
 	struct TNo *p, *ant, *novo;
@@ -29,20 +37,36 @@ void insere_ordenado(struct TNo **l, int x) {
 		*l = novo;
 }
 
+
+bool insere_ordenado_recursivo(struct TNo **lista, int x) {
+	if (*lista == NULL || (*lista)->info > x) {
+		struct TNo *novo;
+		novo = (struct TNo*)malloc(sizeof(struct TNo));
+		if (novo == NULL) {
+			return false;
+		}
+		novo->info = x;
+		novo->prox = *lista;
+		*lista = novo;
+		return true;
+	}
+	insere_ordenado_recursivo(&(*lista)->prox, x);
+}
+
+
 void escreve_lista(struct TNo* l) {
-	struct TNo* p;
 	int cont;
 
 	cont = 0;
-	p = l;
-	while(p) {
+	while(l) {
 		cont++;
-		printf("%d elemento: %d\n", cont, p->info);
-		p = p->prox;
+		printf("%d elemento: %d\n", cont, l->info);
+		l = l->prox;
 	}
 	printf("\n");
 
 }
+
 
 void destroi(struct TNo **l) {
     struct TNo *lixo;
@@ -52,6 +76,7 @@ void destroi(struct TNo **l) {
         free(lixo);
     }
 }
+
 
 void insere(struct TNo **lista, float x) {
     struct TNo *novo;
@@ -66,5 +91,34 @@ void insere(struct TNo **lista, float x) {
         while (p->prox != NULL)
             p = p->prox;
         p->prox = novo;
+    }
+}
+
+
+void escreve_lista_duplamente(struct Tno *l) {
+    int cont = 1;
+    struct Tno *atual = l;
+    
+    while (atual != NULL) {
+        printf("%d elemento: %d\n", cont, atual->info);
+        atual = atual->dir;
+        cont++;
+    }
+    printf("\n");
+}
+
+
+void escreve_lista_duplamente_inverso(struct Tno *lista) {
+
+    struct Tno *atual = lista;
+    while (atual->dir != NULL) {
+        atual = atual->dir;
+    }
+
+    int cont = 1;
+    while (atual != NULL) {
+        printf("%d elemento (inverso): %d\n", cont, atual->info);
+        atual = atual->esq;
+        cont++;
     }
 }
--- questoes.c
+++ questoes.c
@@ -7,8 +7,17 @@ struct TNo {
     struct TNo *prox;     
 };
 
+struct Tno {
+	int info;
+	struct Tno *esq;
+	struct Tno *dir;
+};
+
 void insere_ordenado(struct TNo **l, int x);
+void insere_ordenado_recursivo(struct TNo **l, int x);
 void escreve_lista(struct TNo *l);
+void escreve_lista_duplamente(struct Tno *l);
+void escreve_lista_duplamente_inverso(struct Tno *lista);
 void destroi(struct TNo **l);
 void insere(struct TNo **lista, float x);
 
@@ -53,26 +62,6 @@ bool verifica_se_x_aparece_antes_de_y(struct TNo *lista, float x, float y) {
 }
 
 
-// Questão 13 - Iterativa
-void troca_ultimo_com_primeiro(struct TNo **lista) {
-	struct TNo *penultimo, *ultimo;
-	ultimo = *lista;
-	if (ultimo != NULL && ultimo->prox != NULL) {
-		penultimo = NULL;
-		while (ultimo->prox != NULL) {
-			penultimo = ultimo;
-			ultimo = ultimo->prox;
-		}
-		penultimo->prox = *lista;
-		ultimo->prox = (*lista)->prox;
-		(*lista)->prox = NULL;
-		*lista = ultimo;
-	} else {
-		return; // não precisa disso no zezinhol
-	}
-}
-
-
 // Questão 12 - Iterativa
 bool remove_valores_maiores_ou_iguais(struct TNo **lista, int x) {
 	struct TNo *ant, *p, *lixo;
@@ -100,7 +89,7 @@ bool remove_valores_maiores_ou_iguais(struct TNo **lista, int x) {
 
 
 // Questão 12 - Recursiva
-bool remover(struct TNo **l, int x) {
+bool remove_maiores_ou_iguais_recursiva(struct TNo **l, int x) {
 	struct TNo *lixo;
 	if (*l == NULL) {
 		return false;
@@ -109,9 +98,9 @@ bool remover(struct TNo **l, int x) {
 		lixo = *l;
 		*l = (*l)->prox;
 		free(lixo);
-		return remover(l, x) || true;
+		return remove_maiores_ou_iguais_recursiva(l, x) || true;
 	}
-	return remover(&(*l)->prox, x);
+	return remove_maiores_ou_iguais_recursiva(&(*l)->prox, x);
 }
 
 
@@ -137,7 +126,7 @@ struct TNo* copialista(struct TNo *l) {
 
 
 // Questão 15 - Recursiva by Kauan
-struct TNo* copia(struct TNo *l) {
+struct TNo* copia_lista_recursiva(struct TNo *l) {
 	struct TNo *l2, *novo;
 	if (l == NULL) {
 		l2 = NULL;
@@ -145,7 +134,7 @@ struct TNo* copia(struct TNo *l) {
 	}
 	novo = (struct TNo*)malloc(sizeof(struct TNo));
 	novo->info = l->info;
-	novo->prox = copia(l->prox);
+	novo->prox = copia_lista_recursiva(l->prox);
 	l2 = novo;
 	printf("Nova lista\n");
 	escreve_lista(l2);
@@ -235,10 +224,10 @@ bool insere_x_no_fim_de_l(struct TNo **l, int x) {
 	if (*l == NULL) {
 		*l = novo;
 	} else {
-		while (p->prox != NULL) {
+		while (p != NULL) {
 			p = p->prox;
 		}
-		p->prox = novo;
+		p = novo;
 	}
 	return true;
 }
@@ -261,123 +250,52 @@ bool insere_x_no_fim_de_l_recursiva(struct TNo **l, int x) {
 }
 
 
-// Questão 13 - Recursiva
-void troca_ultimo_com_primeiro_recursivo(struct TNo **lista) {
-	struct TNo *p, *ultimo;
-	
-	void percorre_h(struct TNo **h) {
-		if ((*h)->prox->prox == NULL) {
-			ultimo = (*h)->prox;
-			ultimo->prox = (*lista)->prox;
-			(*h)->prox = *lista;
-			(*lista)->prox = NULL;
-			*lista = ultimo;
-		} else {
-			percorre_h(&(*h)->prox);
-		}
+// Questão 11 - Recursiva - Arthur
+bool remove_ultimo_arthur(struct TNo **lista) {
+	if (*lista == NULL) {
+		return false;
 	}
-	
-	p = *lista;
-	if (p != NULL && p->prox != NULL){
-		if (p->prox->prox == NULL) {
-			ultimo = p->prox;
-			ultimo->prox = *lista;
-			(*lista)->prox = NULL;
-			*lista = ultimo;
-		} else {
-			percorre_h(&p);
-		}
-	}
-}
-
-
-// Questão 1 - PROVA DRIVE
-bool insere_ordenado_recursivo(struct TNo **l, int x) {
-	struct TNo *novo;
-	if (*l == NULL || (*l)->info > x) {
-		novo = (struct TNo*)malloc(sizeof(struct TNo));
-		if (novo == NULL) {
-			return false;
-		}
-		novo->info = x;
-		novo->prox = *l;
-		*l = novo;
+	if ((*lista)->prox == NULL) {
+		free(*lista);
+		*lista = NULL;
 		return true;
 	}
-	return insere_ordenado_recursivo(&(*l)->prox, x);
+	return remove_ultimo_arthur(&(*lista)->prox);
 }
 
 
-// Questão 2 - PROVA DRIVE - André
-void altere(struct TNo **l) {
-	struct TNo *refer, *p, *a;
-	int aux;
-	a = *l;
-	if (a != NULL) {
-		p = a;
-		while (p->prox != NULL) {
-			p = p->prox;
-		}
-		refer = p;
-		while (a != refer) {
-			aux = a->info;
-			a->info = refer->info;
-			refer->info = aux;
-			if (a->prox == refer) {
-				a = refer;
-			} else {
-				p = a;
-				while (p->prox != refer) {
-					p = p->prox;
-				}
-				refer = p;
-				a = a->prox;
-			}
-		}
-	}
-}
+// bool par_impar_guilherme(struct TNo **par, struct TNo **impar, struct TNo *l) {
+	
+// 	struct TNo *p, *i;
 
-
-// Questão 16 - Iterativa
-void separa_pares_de_impares(struct TNo **lista1, struct TNo **lista2) {
-
-	struct TNo *ant, *p, *f, *novo, *lixo;
-
-	ant = NULL;
-	p = *lista1;
-	f = *lista2;
-
-	while (p != NULL) {
-		if (p->info % 2 != 0) {
-			novo = (struct TNo*)malloc(sizeof(struct TNo));
-			if (novo != NULL) {
-				novo->info = p->info;
-				novo->prox = NULL;
-				if (f == NULL) {
-					*lista2 = novo;
-				} else {
-					f->prox = novo;
-				}
-				lixo = p;
-				if (ant == NULL) {
-					*lista1 = p->prox;
-				} else {
-					ant->prox = p->prox;
-				}
-				p = p->prox;
-				f = novo;
-				free(lixo);
-			}
-		} else {
-			ant = p;
-			p = p->prox;
-		}
-	}
-	escreve_lista(*lista1);
-	escreve_lista(*lista2);
-}
-
-
+// 	void insere() {
+// 		*par = (struct TNo*)malloc(sizeof(struct TNo));
+// 		*impar = (struct TNo*)malloc(sizeof(struct TNo));
+// 		p = *par;
+// 		i = *impar;
+// 		while (l != NULL) {
+// 			if (l->info % 2 == 0) {
+// 				if (p != NULL) {
+// 					p->info = l->info;
+// 					p->prox = NULL;
+// 					p = p->prox;
+// 					p = (struct TNo*)malloc(sizeof(struct TNo));
+// 				}
+// 			} else {
+// 				if (i != NULL) {
+// 					i->info = l->info;
+// 					i->prox = NULL;
+// 					i = i->prox;
+// 					i = (struct TNo*)malloc(sizeof(struct TNo));
+// 				}
+// 			}
+// 			l = l->prox;
+// 		}
+// 	}
+// 	if (l != NULL && *par == NULL && *impar == NULL) {
+// 		insere();
+// 	}
+// }
 
 
 
@@ -403,79 +321,219 @@ bool soma_posicao(struct TNo **l, int p) {
 	
 	bool procura_posicao(struct TNo **n, int pos) {
 		struct TNo *proximo;
-		if (*n == NULL) {
+		if ((*n)->prox == NULL) {
 			return false;
 		}
-		if (pos == p && (*n)->prox != NULL) {
-			proximo = (*n)->prox;
-			proximo->info = (*n)->info + proximo->info;
+		if (pos == p) {
+			(*n)->prox->info = (*n)->info + (*n)->prox->info;
 			return true;
 		}
 		procura_posicao(&(*n)->prox, pos + 1);
 	}
-
+	
 	if (*l == NULL || (*l)->prox == NULL || p <= 0) {
 		return false;
 	}
 	procura_posicao(&(*l), 1);
-
 } 
 
 
-// Questão 11 - Recursiva - Arthur
-bool remove_ultimo_arthur(struct TNo **lista) {
-	if (*lista == NULL) {
-		return false;
-	}
-	if ((*lista)->prox == NULL) {
-		free(*lista);
-		*lista = NULL;
-		return true;
-	}
-	return remove_ultimo_arthur(&(*lista)->prox);
-}
+// Questão 16 - Iterativa
+void separa_pares_de_impares(struct TNo *lista, struct TNo **par, struct TNo **impar) {
+	struct TNo *novo, *par_tail, *impar_tail;
 
+	par_tail = NULL;
+	impar_tail = NULL;
 
-bool par_impar_guilherme(struct TNo **par, struct TNo **impar, struct TNo *l) {
-	
-	struct TNo *p, *i;
-
-	void insere() {
-		*par = (struct TNo*)malloc(sizeof(struct TNo));
-		*impar = (struct TNo*)malloc(sizeof(struct TNo));
-		p = *par;
-		i = *impar;
-		while (l != NULL) {
-			if (l->info % 2 == 0) {
-				if (p != NULL) {
-					p->info = l->info;
-					p->prox = NULL;
-					p = p->prox;
-					p = (struct TNo*)malloc(sizeof(struct TNo));
+	if (*par == NULL && *impar == NULL) {
+		while (lista != NULL) {
+			novo = (struct TNo*)malloc(sizeof(struct TNo));
+			if (novo != NULL) {
+				novo->info = lista->info;
+				novo->prox = NULL;
+				if (lista->info % 2 != 0) {
+					if (*par == NULL) {
+						*par = novo;
+						par_tail = novo;
+					} else {
+						par_tail->prox = novo;
+						par_tail = novo;
 				}
-			} else {
-				if (i != NULL) {
-					i->info = l->info;
-					i->prox = NULL;
-					i = i->prox;
-					i = (struct TNo*)malloc(sizeof(struct TNo));
+				} else {
+					if (*impar == NULL) {
+						*impar = novo;
+						impar_tail = novo;
+					} else {
+						impar_tail->prox = novo;
+						impar_tail = novo;
+					}
 				}
+				lista = lista->prox;
 			}
-			l = l->prox;
 		}
 	}
-	if (l != NULL && *par == NULL && *impar == NULL) {
-		insere();
+}
+
+
+// Questão 4 - Prova Drive - by Deepseek
+
+// struct TNo* somarPolinomios(struct TNo *p1, struct TNo *p2) {
+//     struct TNo *resultado = NULL;
+//     struct TNo *atual = NULL;
+    
+//     while (p1 != NULL || p2 != NULL) {
+//         struct TNo *novo = NULL;
+//         int exp1, exp2;
+        
+//         // Determinar exp1 sem operador ternário
+//         if (p1 != NULL) {
+//             exp1 = p1->expoente;
+//         } else {
+//             exp1 = -1;
+//         }
+        
+//         // Determinar exp2 sem operador ternário
+//         if (p2 != NULL) {
+//             exp2 = p2->expoente;
+//         } else {
+//             exp2 = -1;
+//         }
+        
+//         if (exp1 > exp2) {
+//             novo = (struct TNo*)malloc(sizeof(struct TNo));
+//             if (novo == NULL) return NULL;
+//             novo->coeficiente = p1->coeficiente;
+//             novo->expoente = p1->expoente;
+//             novo->prox = NULL;
+//             p1 = p1->prox;
+//         } 
+//         else if (exp2 > exp1) {
+//             novo = (struct TNo*)malloc(sizeof(struct TNo));
+//             if (novo == NULL) return NULL;
+//             novo->coeficiente = p2->coeficiente;
+//             novo->expoente = p2->expoente;
+//             novo->prox = NULL;
+//             p2 = p2->prox;
+//         } 
+//         else {
+//             float soma = p1->coeficiente + p2->coeficiente;
+//             if (soma != 0) {
+//                 novo = (struct TNo*)malloc(sizeof(struct TNo));
+//                 if (novo == NULL) return NULL;
+//                 novo->coeficiente = soma;
+//                 novo->expoente = p1->expoente;
+//                 novo->prox = NULL;
+//             }
+//             p1 = p1->prox;
+//             p2 = p2->prox;
+//         }
+        
+//         if (novo != NULL) {
+//             if (resultado == NULL) {
+//                 resultado = novo;
+//                 atual = novo;
+//             } else {
+//                 atual->prox = novo;
+//                 atual = novo;
+//             }
+//         }
+//     }
+    
+//     return resultado;
+// }
+
+
+// Questão 1 - Prova Drive
+bool insere_ordenado_recursivo_duplamente(struct Tno **lista, int x) {
+    if (*lista == NULL || (*lista)->dir == NULL) {
+        struct Tno *novo = (struct Tno*)malloc(sizeof(struct Tno));
+        if (novo == NULL) {
+            return false;
+        }
+        novo->info = x;
+        novo->dir = NULL;
+		if (*lista != NULL) {
+			novo->esq = *lista;
+			(*lista)->dir = novo;
+		} else {
+			novo->esq = NULL;
+			*lista = novo;
+		}
+        return true;
+    }
+    
+    if ((*lista)->info > x) {
+        struct Tno *novo = (struct Tno*)malloc(sizeof(struct Tno));
+        if (novo == NULL) {
+            return false;
+        }
+
+        novo->info = x;
+        novo->dir = *lista;
+        novo->esq = (*lista)->esq;
+        
+        if ((*lista)->esq != NULL) {
+            (*lista)->esq->dir = novo;
+        }
+
+        (*lista)->esq = novo;
+        
+        if (novo->esq == NULL) {
+            *lista = novo;
+        }
+        
+        return true;
+    }
+    
+    return insere_ordenado_recursivo_duplamente(&(*lista)->dir, x);
+}
+
+
+// Questão 16 - Recursiva - by Giovana
+void separa_pares_de_impares_recursiva(struct TNo *lista, struct TNo **par, struct TNo **impar) {
+	struct TNo *novo;
+
+	if (lista == NULL) {
+		*par = NULL;
+		*impar = NULL;
+	} else {
+		if (lista->info % 2 == 0) {
+			novo = (struct TNo*)malloc(sizeof(struct TNo));
+			if (novo != NULL) {
+				novo->info = lista->info;
+				novo->prox = NULL;
+				*par = novo;
+			}
+		} else {
+			novo = (struct TNo*)malloc(sizeof(struct TNo));
+			if (novo != NULL) {
+				novo->info = lista->info;
+				novo->prox = NULL;
+				*impar = novo;
+			}
+		}
+
+		if (lista->info % 2 == 0 && *par != NULL) {
+			separa_pares_de_impares_recursiva(lista->prox, &((*par)->prox), impar);
+		} else {
+			if (lista->info % 2 != 0 && *impar != NULL) {
+				separa_pares_de_impares_recursiva(lista->prox, par, &((*impar)->prox));
+			} else {
+				separa_pares_de_impares_recursiva(lista->prox, par, impar);
+			}
+		}
 	}
 }
+
 
 
 int main() {
 	struct TNo *lista = NULL;
-	struct TNo *lista2 = NULL;
-	
-	struct TNo *impar = NULL;
+
 	struct TNo *par = NULL;
+	struct TNo *impar = NULL;
+
+	struct Tno *lista2 = NULL;
 	
 	insere_ordenado_recursivo(&lista, 5);
 	insere_ordenado_recursivo(&lista, 3);
@@ -485,25 +543,21 @@ int main() {
 	insere_ordenado_recursivo(&lista, 1);
 
 	escreve_lista(lista);
-	par_impar_guilherme(&par, &impar, lista); 
+
+	separa_pares_de_impares_recursiva(lista, &par, &impar);
+
 
 	escreve_lista(par);
 	escreve_lista(impar);
 
-	destroi(&lista);
 
+	destroi(&lista);
 
     return 0;
 }
 
+
 // Faltam:
-// 15; 16 - Recursiva;
-
-// Faltam - Prova Passada:
-//
-
-// Faltam - Prova Drive:
-// 4;
-
-// Arrumar:
-// (testar a de inverter, a de separar impar e par, a 14-recursiva)
+// Olhar a 15 recursiva do Kauan, a 16 iterativa, a 1 do drive, a 13 do zezinho, a 12
+// Falta a 16 recursiva
+// Revisar
